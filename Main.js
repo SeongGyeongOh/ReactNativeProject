a14f92e5f06506e2d22d551d2429e18de28c5c52
SeongGyeong <deborahoh94@gmail.com>
2020-10-05 10:00:44 +0900

    git test

---
+++ Main.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import { Text, View } from 'react-native';
+
+export default class Main extends Component{
+    render(){
+        return(
+            <View>
+                <Text>깃헙 테스트!!</Text>
+            </View>
+        );
+    }
+}
